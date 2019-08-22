9642661f5c616cd2c5977b52593f3e8df3d1740e
Jochen Breuer <brejoc@gmail.com>
2019-08-22 14:33:09 +0200

    Initial commit

---
+++ script/dist_outs.py
@@ -0,0 +1,10 @@
+import distro
+
+print(distro.linux_distribution())
+print("========================================")
+print("id:       {}".format(distro.id()))
+print("name:     {}".format(distro.name()))
+print("like:     {}".format(distro.like()))
+print("name:     {}".format(distro.name()))
+print("version:  {}".format(distro.version()))
+print("")
